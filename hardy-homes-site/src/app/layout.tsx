cd37f90b5ac750819f5cf886654e1ff1901e0c1a
Brian Hardy <bc.hardy47@gmail.com>
2026-09-05 20:53:47 -0600

    Fix Hardy Homes production font loading

--- hardy-homes-site/src/app/layout.tsx
+++ hardy-homes-site/src/app/layout.tsx
@@ -30,6 +30,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Barlow:ital,wght@0,400;0,500;0,600;1,400&display=swap"
+        />
+      </head>
       <body>
         <HardyHomesNav />
         {children}
